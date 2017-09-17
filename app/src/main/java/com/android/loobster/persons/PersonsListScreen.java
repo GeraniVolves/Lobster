0414ea124ff30d7083d33303b9d80ff321cb90dd
Ilya Eremin <partissanei@gmail.com>
2017-09-17 15:00:18 +0300

    refactoring

--- app/src/main/java/com/android/loobster/persons/PersonsListScreen.java
+++ app/src/main/java/com/android/loobster/persons/PersonsListScreen.java
@@ -1,6 +1,5 @@
 package com.android.loobster.persons;
 
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -9,10 +8,6 @@ import android.support.v7.widget.RecyclerView;
 import com.android.loobster.R;
 
 public class PersonsListScreen extends Activity {
-
-    private RecyclerView               mRecyclerView;
-    private RecyclerView.Adapter       mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
 
     private String[] myDataset = new String[]{
         "Country",
@@ -34,20 +29,8 @@ public class PersonsListScreen extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.to_do_list_screen);
-        mRecyclerView = findViewById(R.id.list);
-
-        // use this setting to improve performance if you know that changes
-        // in content do not change the layout size of the RecyclerView
-        mRecyclerView.setHasFixedSize(true);
-
-        // use a linear layout manager
-        mLayoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(mLayoutManager);
-
-        // specify an adapter (see also next example)
-        mAdapter = new PersonsListAdapter(myDataset);
-        mRecyclerView.setAdapter(mAdapter);
+        RecyclerView uiList = findViewById(R.id.list);
+        uiList.setLayoutManager(new LinearLayoutManager(this));
+        uiList.setAdapter(new PersonsListAdapter(myDataset));
     }
-
-
 }
