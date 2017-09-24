c313f5c9ee7bd04b825c57e782cccb4b562ad531
GeraniVolves <eremina.kseniya@gmail.com>
2017-09-24 14:15:39 +0300

    create navigation

--- app/src/main/java/com/android/loobster/LoginScreen.java
+++ app/src/main/java/com/android/loobster/LoginScreen.java
@@ -8,8 +8,8 @@ public class LoginScreen extends AppCompatActivity {
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.screen_login);
-        findViewById(R.id.sign_up).setOnClickListener(l -> Navigator.signup(this));
-        findViewById(R.id.forgot_password).setOnClickListener(l -> Navigator.recoverAccount(this));
-        findViewById(R.id.login).setOnClickListener(l -> Navigator.create(this));
+        findViewById(R.id.sign_up).setOnClickListener(l -> NavigateTo.signup(this));
+        findViewById(R.id.forgot_password).setOnClickListener(l -> NavigateTo.recoverAccount(this));
+        findViewById(R.id.login).setOnClickListener(l -> NavigateTo.main(this));
     }
 }
