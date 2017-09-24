5eabcf60e8b66f3a2e6ebca54e1e5fa84da95fae
GeraniVolves <eremina.kseniya@gmail.com>
2017-09-24 11:15:59 +0300

    change colors, add screens "recovery account" and "create password" for Facebook

---
+++ app/src/main/java/com/android/loobster/LogInActivity.java
@@ -0,0 +1,12 @@
+package com.android.loobster;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LogInActivity extends Activity {
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.screen_log_in);
+    }
+}
---
+++ app/src/main/java/com/android/loobster/SignUpActivity.java
@@ -0,0 +1,14 @@
+package com.android.loobster;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class SignUpActivity extends Activity {
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.screen_sign_up);
+    }
+}
+
